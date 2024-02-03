a0e156a2adda215494edb11099a33204105ea751
rahulramagiri <rahulramagiri8@gmail.com>
2024-02-03 21:11:16 +0530

    git angular setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Display: "Fascinate Inline",
+        Body: "Poppins",
+      },
+    },
+  },
+  plugins: [],
+}
